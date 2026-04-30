fcf4d1dc23d7ef4e70d60c8773c6535b530d4d63
Darun Karas Abir <darun15-14188@diu.edu.bd>
2026-04-30 22:48:58 +0600

    model view and controller added

---
+++ BookApi/Models/Books.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Extensions.Hosting;
+
+namespace BookApi.Models
+{
+	public class Books
+	{
+		
+		public int Id { get; set; }
+		public string? Title { get; set; }
+		public string Author { get; set; } = "";
+		public int PublicationYear { get; set; }
+		public bool IsAvaialable { get; set; }
+		public string? CallNumber { get; set; }
+
+
+	}
+}
+
+
