ab906130c7d8e6dfb6a2dfb10add9fb50de25bbb
Darun Karas Abir <darun15-14188@diu.edu.bd>
2026-04-30 21:44:08 +0600

    first commit

---
+++ BookApi/Controllers/ValueController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace BookApi.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValueController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new List<string> { "Apple", "Banana" };
+        }
+
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "This value is:" +id;
+        }
+    }
+}
+
